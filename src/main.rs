5b0ad5c8864b0d8dc69380430d8aeeda695c2315
Menno Finlay-Smits <hello@menno.io>
2024-07-29 22:48:40 +1200

    notify extracted to module

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
+mod notify;
 mod watch;
 
-use std::time::Duration;
-
 use color_eyre::eyre::Result;
-use dbus::arg::messageitem::MessageItemArray;
-use dbus::blocking::Connection;
 
 // TODO
 // - github
-// - split out watching in a module, make it take a notify fn
 // - figure out icons
 // - report dead services on startup
 // - release
@@ -16,41 +12,7 @@ use dbus::blocking::Connection;
 // - configurable delay
 // - ignore some units
 
-fn notify(unit_name: &str, result: &str) {
-    let conn = Connection::new_session().expect("D-Bus connection failed");
-
-    let proxy = conn.with_proxy(
-        "org.freedesktop.Notifications",
-        "/org/freedesktop/Notifications",
-        Duration::from_millis(5000),
-    );
-
-    let actions = MessageItemArray::new(vec![], "as".into()).unwrap();
-    let hints = MessageItemArray::new(vec![], "a{sv}".into()).unwrap();
-
-    let result: Result<(), dbus::Error> = proxy.method_call(
-        "org.freedesktop.Notifications",
-        "Notify",
-        (
-            "sdshout",
-            0u32,
-            "", // app_icon
-            format!("Local unit {} is {}", unit_name, result),
-            "",
-            actions,
-            hints,
-            15000i32, // expire timeout (ms)
-        ),
-    );
-    match result {
-        Ok(_) => {}
-        Err(err) => {
-            eprintln!("notify failed: '{}'", err);
-        }
-    }
-}
-
 fn main() -> Result<()> {
     color_eyre::install()?;
-    watch::watch_units(notify)
+    watch::watch_units(notify::notify)
 }
---
+++ src/notify.rs
@@ -0,0 +1,39 @@
+use std::time::Duration;
+
+use color_eyre::eyre::Result;
+use dbus::arg::messageitem::MessageItemArray;
+use dbus::blocking::Connection;
+
+pub fn notify(unit_name: &str, result: &str) {
+    let conn = Connection::new_session().expect("D-Bus connection failed");
+
+    let proxy = conn.with_proxy(
+        "org.freedesktop.Notifications",
+        "/org/freedesktop/Notifications",
+        Duration::from_millis(5000),
+    );
+
+    let actions = MessageItemArray::new(vec![], "as".into()).unwrap();
+    let hints = MessageItemArray::new(vec![], "a{sv}".into()).unwrap();
+
+    let result: Result<(), dbus::Error> = proxy.method_call(
+        "org.freedesktop.Notifications",
+        "Notify",
+        (
+            "sdshout",
+            0u32,
+            "", // app_icon
+            format!("Local unit {} is {}", unit_name, result),
+            "",
+            actions,
+            hints,
+            15000i32, // expire timeout (ms)
+        ),
+    );
+    match result {
+        Ok(_) => {}
+        Err(err) => {
+            eprintln!("notify failed: '{}'", err);
+        }
+    }
+}
