da6b255dc85f359f07ae08109a4e5872b2785942
Menno Finlay-Smits <hello@menno.io>
2024-07-29 23:28:46 +1200

    Moved TODO items to GitHub

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,6 @@ mod watch;
 
 use eyre::Result;
 
-// TODO
-// - github
-// - figure out icons
-// - report dead services on startup
-// - release
-// - rate limiting and/or aggregation
-// - configurable delay
-// - ignore some units
-
 fn main() -> Result<()> {
     watch::watch_units(notify::notify)
 }
